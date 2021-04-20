061d2c994add6f256db0e9b4a1b9afe96c13dc8a
MMatyukha <77583720+MMatyukha@users.noreply.github.com>
2021-04-20 20:12:53 +0300

    Update h_w.py

--- less_6/h_w.py
+++ less_6/h_w.py
@@ -16,7 +16,7 @@ try:
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,".button"))).click()
     driver_d.get('http://test-stage.geekbrains.ru:8080/timer')
     like_locator = (By.CSS_SELECTOR, "label.#demo")
-    wait.until(EC.text_to_be_present_in_element(like_locator, '100'))
+    wait.until(EC.text_to_be_present_in_element(like_locator,'100'))
     driver_d.find_element(By.CSS_SELECTOR,".button").click()
     WebDriverWait(driver_d, 10).until(EC.alert_is_present())
     alert = driver_d.switch_to.alert
